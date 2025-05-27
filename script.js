27d5422707719dc1129e5a6fabdc849377fd72a6
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-05-28 04:46:48 +0530

    task add functionaluty added

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-let allElem = document.querySelectorAll('.elem')
+function opencard(){
+    let allElem = document.querySelectorAll('.elem')
 
-let forElem = document.querySelectorAll('.forElem')
+    let forElem = document.querySelectorAll('.forElem')
 
-let back = document.querySelectorAll('.back')
+    let back = document.querySelectorAll('.back')
 
-allElem.forEach(function(elem){
-    elem.addEventListener('click',function(){
+    allElem.forEach(function(elem){
+        elem.addEventListener('click',function(){
 
-        document.querySelector("#main1").style.display = "none";
-        document.querySelector("#main2").style.display = "block";
-        // forElem.forEach(f => f.style.display = 'none');  
-        forElem[elem.id].style.display = 'block';  
-    })   
-})
-
-back.forEach(function(btn){
-    btn.addEventListener('click',function(){
-        document.querySelector("#main1").style.display = "flex";
-        document.querySelector("#main2").style.display = "none";
-        document.querySelectorAll('.forElem').forEach(f => f.style.display = 'none');
+            document.querySelector("#main1").style.display = "none";
+            document.querySelector("#main2").style.display = "block";
+            // forElem.forEach(f => f.style.display = 'none');  
+            forElem[elem.id].style.display = 'block';  
+        })   
     })
- 
-})
+
+    back.forEach(function(btn){
+        btn.addEventListener('click',function(){
+            document.querySelector("#main1").style.display = "flex";
+            document.querySelector("#main2").style.display = "none";
+            document.querySelectorAll('.forElem').forEach(f => f.style.display = 'none');
+        })
+    
+    })
+
+}
+
+opencard()
